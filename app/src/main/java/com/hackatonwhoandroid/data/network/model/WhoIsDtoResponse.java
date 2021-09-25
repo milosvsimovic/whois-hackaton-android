5dbb2898b1803532765946ff448d2b478ca41a12
milosvsimovic <milosvsimovic@gmail.com>
2021-09-25 23:30:19 +0200

    EMPTY bato

--- app/src/main/java/com/hackatonwhoandroid/data/network/model/WhoIsDtoResponse.java
+++ app/src/main/java/com/hackatonwhoandroid/data/network/model/WhoIsDtoResponse.java
@@ -1,10 +1,14 @@
 package com.hackatonwhoandroid.data.network.model;
 
+import android.content.res.Resources;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.hackatonwhoandroid.R;
+import com.hackatonwhoandroid.domain.model.DomainStatus;
 import com.hackatonwhoandroid.domain.model.Message;
 
-import org.joda.time.DateTime;
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -31,15 +35,25 @@ public class WhoIsDtoResponse {
     @Mapper
     public static abstract class Mappers {
 
-        @Mapping(target = "body", expression = "java(convertToBody(response))")
+        @Mapping(target = "body", expression = "java(convertToBody(response, resources))")
         @Mapping(target = "type", expression = "java(getType())")
         @Mapping(target = "timeStamp", ignore = true)
         @Mapping(target = "createdByUser", ignore = true)
         @Mapping(target = "favorite", ignore = true)
-        public abstract Message mapToMessage(WhoIsDtoResponse response);
+        public abstract Message mapToMessage(WhoIsDtoResponse response, Resources resources);
 
-        String convertToBody(WhoIsDtoResponse response) {
-            return response.toString();
+        String convertToBody(WhoIsDtoResponse response, Resources resources) {
+            String body = StringUtils.EMPTY;
+            DomainStatus domainStatus = DomainStatus.valueOf(response.domainStatus);
+            if (DomainStatus.ACTIVE == domainStatus) {
+                body = String.format(resources.getString(R.string.chat_domain_is_already_registred),
+                        response.getDomainName(), response.getRegistrationDate(), response.getExpirationDate());
+
+            } else if (DomainStatus.INACTIVE == domainStatus) {
+                body = String.format(resources.getString(R.string.chat_domain_is_available),
+                        response.getDomainName());
+            }
+            return body;
         }
 
         Message.Type getType() {
